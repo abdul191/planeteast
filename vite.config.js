72104bce656385ca5bfbedc70a0d396fd0b80010
abdul191006 <abdul191006@gmail.com>
2025-04-22 21:45:57 +0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ghPages from 'vite-plugin-gh-pages'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  ghPages()
-  ],
-  base: '/planeteast/'
+  plugins: [react()],
+  base: '/planeteast/', // this MUST match your GitHub repo name
 })
