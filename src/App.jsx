43c507423e4c3fd620ad862b98edc4f018497aa4
abdul191006 <abdul191006@gmail.com>
2025-04-22 21:05:55 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <button className="text-3xl font-bold text-red-500 underline text-center">Hello there</button>
+    </>
+  )
+}
+
+export default App
